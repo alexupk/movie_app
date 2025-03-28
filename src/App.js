24b006388fdab4a3f2c149e30a3d8fe6c619e6f9
Oleksandr Nakorenok <alexandrnakorenok@gmail.com>
2025-03-28 16:30:21 +0200

    next

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  getMoves = async() => {
+    const { data: { data: { movies} } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading : false });
+  }
+
+  componentDidMount() {
+    this.getMoves();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+    return <section className='container'> 
+      {isLoading ? <div className='loader'>
+        <span className='loader__text'>Загрузка...</span>
+      </div> : movies.map(movie => {
+        
+        return <div className='movies'>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres} />
+        </div>
+      })}
+    </section>;
+  }
 }
 
 export default App;
