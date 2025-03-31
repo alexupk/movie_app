6a820b3236e4808fe54d1e30563afc376c48006f
Oleksandr Nakorenok <alexandrnakorenok@gmail.com>
2025-03-31 13:38:51 +0300

    css

--- src/App.js
+++ src/App.js
@@ -21,23 +21,29 @@ class App extends React.Component {
 
   render() {
     const {isLoading, movies} = this.state;
-    return <section className='container'> 
-      {isLoading ? <div className='loader'>
-        <span className='loader__text'>Загрузка...</span>
-      </div> : movies.map(movie => {
-        
-        return <div className='movies'>
-          <Movie
-            key={movie.id}
-            id={movie.id}
-            year={movie.year}
-            title={movie.title}
-            summary={movie.summary}
-            poster={movie.medium_cover_image}
-            genres={movie.genres} />
-        </div>
-      })}
-    </section>;
+    return (
+      <section className='container'>
+        {isLoading ? (
+          <div className='loader'>
+            <span className='loader__text'>Загрузка...</span>
+          </div>
+        ) : (
+          <div className='movies'>
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
